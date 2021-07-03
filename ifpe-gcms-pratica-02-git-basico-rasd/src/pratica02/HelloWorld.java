e0e45218e1eb5e21d9b4e7e1b4bfb6dd84e3c550
matias <matias@Notebook>
2021-07-03 10:58:41 -0300

    Teste

--- ifpe-gcms-pratica-02-git-basico-rasd/src/pratica02/HelloWorld.java
+++ ifpe-gcms-pratica-02-git-basico-rasd/src/pratica02/HelloWorld.java
@@ -3,7 +3,7 @@ package pratica02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("############");
+		System.out.println("######");
 		System.out.println("######\nHello \nWorld\n######");
 
 	}
